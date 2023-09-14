ee6b71ebe14eed3e40e855ef8f8336ea18d4f476
sunghyun1014 <sunghyun1014@naver.com>
2023-09-14 15:19:12 +0900

    커밋 4 곱하기 기능 추가

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ def sub_func(n1,n2):
     res=n1-n2
     return res
 
+def mul_func(n1,n2):
+    res=n1*n2
+    return res
+
+
+
 
 
 num1=100
@@ -17,3 +23,6 @@ print(num1,'+',num2,'=',result)
 
 result=sub_func(num1,num2)
 print(num1,'-',num2,'=',result)
+
+result=mul_func(num1,num2)
+print(num1,'*',num2,'=',result)
