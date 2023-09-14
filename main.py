078bfbe02bedc74b2c04a04fa7a1eefc3a93b8ba
sunghyun1014 <sunghyun1014@naver.com>
2023-09-14 17:19:13 +0900

    커밋 6 제곱 함수 추가

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ def div_func(n1,n2):
     res=n1/n2
     return res
 
+def dou_func(n1,n2):
+    res=n1**n2
+    return res
+
 
 num1=100
 num2=200
@@ -31,3 +35,6 @@ print(num1,'*',num2,'=',result)
 
 result=div_func(num1,num2)
 print(num1,'/',num2,'=',result)
+
+result=dou_func(num1,num2)
+print(num1,'**',num2,'=',result)
