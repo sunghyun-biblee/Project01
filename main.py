775f2f7e52f46c151891b1d42cceb7cd05232687
sunghyun1014 <sunghyun1014@naver.com>
2023-09-14 15:20:08 +0900

    커밋 5 나누기 기능 추가

--- main.py
+++ main.py
@@ -11,7 +11,9 @@ def mul_func(n1,n2):
     return res
 
 
-
+def div_func(n1,n2):
+    res=n1/n2
+    return res
 
 
 num1=100
@@ -26,3 +28,6 @@ print(num1,'-',num2,'=',result)
 
 result=mul_func(num1,num2)
 print(num1,'*',num2,'=',result)
+
+result=div_func(num1,num2)
+print(num1,'/',num2,'=',result)
